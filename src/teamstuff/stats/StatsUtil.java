69b0ad6c4fa7985cfb67b49e7c8ffa264326c21a
Roberto Lass <lassroberto@gmail.com>
2016-07-24 15:38:28 +0200

    testint STats

---
+++ src/teamstuff/stats/StatsUtil.java
@@ -0,0 +1,16 @@
+package teamstuff.stats;
+/**
+ * 
+ * @author Roberto
+ *
+ * The Function Utility Class for the Stats
+ */
+public class StatsUtil {
+	
+	public static int sumerizerArenaVisitors(){
+		int resultVisitors = 0;
+		
+		return resultVisitors;
+	}
+
+}
